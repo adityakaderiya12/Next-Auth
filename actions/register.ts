9882cf6d5cf8a1f253099f27b4932aa850679920
adityakaderiya12 <adityakaderiya12@gmail.com>
2024-03-07 00:07:04 +0545

    NVA-10 Reigster form with routing, server action and schema for validation

---
+++ actions/register.ts
@@ -0,0 +1,12 @@
+"use server";
+import { RegisterSchema } from "@/schemas";
+import * as z from "zod";
+export const register = async (values: z.infer<typeof RegisterSchema>) => {
+  const validateFields = RegisterSchema.safeParse(values);
+
+  if (!validateFields.success) {
+    return { error: "Invalid Fields!" };
+  }
+
+  return { success: "Email Sent!" };
+};
