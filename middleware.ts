a14293efd60995e0220daa2e9d752e31c0b20ba3
adityakaderiya12 <adityakaderiya12@gmail.com>
2024-03-07 15:19:24 +0545

    NVA-AK-14 Invoking Middleware with specific matcher to route in all pages where user routed with console

---
+++ middleware.ts
@@ -0,0 +1,21 @@
+import { auth } from "./auth";
+
+export default auth((req) => {
+  //req.auth
+  console.log("ROUTE:", req.nextUrl.pathname);
+});
+
+export const config = {
+  matcher: [
+    /*
+     * Match all request paths except for the ones starting with:
+     * - api (API routes)
+     * - _next/static (static files)
+     * - _next/image (image optimization files)
+     * - favicon.ico (favicon file)
+     */
+    "/((?!.+\\.[\\w]+$|_next).*)",
+    "/",
+    "/(api|trpc)(.*)",
+  ],
+};
