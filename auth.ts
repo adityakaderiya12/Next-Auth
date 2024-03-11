c2cae2e01bea5b8202b44758f8c8ba07aafdf592
adityakaderiya12 <adityakaderiya12@gmail.com>
2024-03-11 14:21:05 +0545

    NAP-21-AK User Role model implementation in session and debugging the code

--- auth.ts
+++ auth.ts
@@ -1,7 +1,9 @@
+import NextAuth from "next-auth";
+
 import authConfig from "@/auth.config";
 import { db } from "@/lib/db";
 import { PrismaAdapter } from "@auth/prisma-adapter";
-import NextAuth from "next-auth";
+import { getUserById } from "./data/user";
 
 export const {
   handlers: { GET, POST },
@@ -10,13 +12,26 @@ export const {
   signOut,
 } = NextAuth({
   callbacks: {
-    async session({ token, session }) {
+    async session({ token, session }: any) {
+      console.log({
+        sessionToken: token,
+      });
       if (token.sub && session.user) {
         session.user.id = token.sub;
       }
+      if (token.role && session.user) {
+        session.user.role = token.role as "ADMIN" | "USER";
+      }
       return session;
     },
+
     async jwt({ token }) {
+      if (!token.sub) return token;
+
+      const existingUser = await getUserById(token.sub);
+      if (!existingUser) return token;
+      token.role = existingUser.role;
+
       return token;
     },
   },
