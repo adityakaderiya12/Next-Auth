ce9dac0f26dd4a024bc9d11c9f5996f1a1bda453
adityakaderiya12 <adityakaderiya12@gmail.com>
2024-03-11 12:05:20 +0545

    NAP-16-AK Implement Logout Functionality and redirecting towards sign in page

--- app/(protected)/settings/page.tsx
+++ app/(protected)/settings/page.tsx
@@ -1,8 +1,22 @@
-import { auth } from "@/auth";
+import { auth, signOut } from "@/auth";
 import React from "react";
+
 const Settings = async () => {
   const session = await auth();
-  return <div>{JSON.stringify(session)}</div>;
+
+  return (
+    <div>
+      {JSON.stringify(session)}
+      <form
+        action={async () => {
+          "use server";
+          await signOut();
+        }}
+      >
+        <button type="submit">Sign out</button>
+      </form>
+    </div>
+  );
 };
 
 export default Settings;
