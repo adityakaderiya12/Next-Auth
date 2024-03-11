873722b098857f091bd12f0a790bf39c60b37f55
adityakaderiya12 <adityakaderiya12@gmail.com>
2024-03-11 13:25:52 +0545

    NAV-20-AK Added UserRole in Database using ORM Prisma and set session User Id in the token

--- auth.ts
+++ auth.ts
@@ -11,14 +11,12 @@ export const {
 } = NextAuth({
   callbacks: {
     async session({ token, session }) {
-      console.log({
-        sessionToken: token,
-        session,
-      });
+      if (token.sub && session.user) {
+        session.user.id = token.sub;
+      }
       return session;
     },
     async jwt({ token }) {
-      console.log({ token });
       return token;
     },
   },
